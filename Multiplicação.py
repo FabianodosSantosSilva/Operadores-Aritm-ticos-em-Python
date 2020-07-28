ae62e1a61224598642f30f78aa12a71d893f4dbd
Fabiano dos Santos <ftrayder@gmail.com>
2020-07-28 16:44:07 -0300

    Exwmplo de multiplicador inserido

---
+++ Multiplicação.py
@@ -0,0 +1,6 @@
+''' Exemplo simples do uso do multiplicador em Python. '''
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro: '))
+mult = n1 * n2
+print(f'O produto multiplicação entre {n1} e {n2} é {mult}.')
