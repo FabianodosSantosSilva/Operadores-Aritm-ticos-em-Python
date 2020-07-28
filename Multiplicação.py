3c189e3beb12d0b4a7093349d2b6bbdae61a38f2
Fabiano dos Santos <ftrayder@gmail.com>
2020-07-28 16:45:52 -0300

    Acerto da linha 6

--- Multiplicação.py
+++ Multiplicação.py
@@ -3,4 +3,4 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro: '))
 mult = n1 * n2
-print(f'O produto multiplicação entre {n1} e {n2} é {mult}.')
+print(f'O produto da multiplicação entre {n1} e {n2} é {mult}.')
