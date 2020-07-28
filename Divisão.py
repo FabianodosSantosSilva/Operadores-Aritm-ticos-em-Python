432e265ff3428e264b1098f426f817bb809f0e1d
Fabiano dos Santos <ftrayder@gmail.com>
2020-07-28 17:11:33 -0300

    Exemplo de divisão inserido

---
+++ Divisão.py
@@ -0,0 +1,6 @@
+''' Exemplo simples de divisão em Python. '''
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro: '))
+div = n1 / n2
+print(f'A divisão entre {n1} e {n2} é {div:.0f}.')
