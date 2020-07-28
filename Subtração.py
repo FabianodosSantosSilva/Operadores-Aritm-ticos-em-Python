afc4d188e4bf8f46353d6cd27b7e5bbdbeae416c
Fabiano dos Santos <ftrayder@gmail.com>
2020-07-28 16:30:49 -0300

    Exemplo de subtração inserido

---
+++ Subtração.py
@@ -0,0 +1,6 @@
+''' Demonstração simples da subtração de dois números. '''
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro: '))
+sub = n1 - n2
+print(f'A subtração entre {n1} e {n2} é {sub}.')
